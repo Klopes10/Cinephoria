f0a57fb76366383022775a7317bd6ba8bfabc257
Klopes10 <kev671007@gmail.com>
2025-09-23 09:25:26 +0200

    correction pour EasyAdmin en deploiement

--- .fly/release.sh
+++ .fly/release.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 set -e
 
-# 1) Publier les assets des bundles (dont EasyAdmin) vers /public/bundles
+# Publier les assets des bundles (EasyAdmin -> /public/bundles/easyadmin)
 php bin/console -n assets:install --symlink --relative
 
-# 2) BDD & cache
+# Compiler AssetMapper UNIQUEMENT si présent (Symfony 7 AssetMapper)
+if php bin/console -q help asset-map:compile >/dev/null 2>&1; then
+  php bin/console -n asset-map:compile
+fi
+
+# Migrations & cache
 php bin/console -n doctrine:migrations:migrate --allow-no-migration --all-or-nothing
 php bin/console -n cache:clear --env=prod
 php bin/console -n cache:warmup --env=prod
