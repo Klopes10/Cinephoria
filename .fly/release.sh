47f5d98ee4399e099ef874ddd31c8784015d2731
Klopes10 <kev671007@gmail.com>
2025-09-23 08:57:52 +0200

    correction visuel

--- .fly/release.sh
+++ .fly/release.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 set -e
 
+# 1) Publier les assets des bundles (dont EasyAdmin) vers /public/bundles
+php bin/console -n assets:install --symlink --relative
+
+# 2) BDD & cache
 php bin/console -n doctrine:migrations:migrate --allow-no-migration --all-or-nothing
 php bin/console -n cache:clear --env=prod
 php bin/console -n cache:warmup --env=prod
